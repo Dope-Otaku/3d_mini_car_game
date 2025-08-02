95f22a25c61c676820aeed73899c23c54b5e76f2
Dope-Otaku <fakeyt909@gmail.com>
2025-08-02 13:44:27 +0530

    we have created another get function just to test

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,10 @@ app.get("/",(req, res)=>{
     res.send("hey how are yaa!")
 })
 
+app.get("/status",(req, res)=>{
+    res.send("we are on status page!")
+})
+
 app.listen(3000, ()=>{
     console.log("server starting on port no: 3000")
 })
