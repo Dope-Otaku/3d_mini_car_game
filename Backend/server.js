91d5abe46765c9a99483d46b8fdbd76f6b64810c
Dope-Otaku <fakeyt909@gmail.com>
2025-08-02 13:43:06 +0530

    introducing express functionalitites

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get("/",(req, res)=>{
+    res.send("hey how are yaa!")
+})
+
 app.listen(3000, ()=>{
     console.log("server starting on port no: 3000")
 })
