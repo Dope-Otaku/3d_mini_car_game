a53d3a762d3e0ad1990ff4ed47c3166902337a40
Dope-Otaku <fakeyt909@gmail.com>
2025-08-03 14:30:00 +0530

    communication from frontend is ready to inititate

--- Frontend/learn/src/App.jsx
+++ Frontend/learn/src/App.jsx
@@ -1,9 +1,21 @@
-
+import axios from "axios"
+import { useState } from "react"
 
 function App() {
+  const [name, setName] = useState("")
+
+ 
+  axios.get('/api/data')
+  .then((response)=>{
+    setName(response.data)
+  })
+  .catch((error)=>{
+    console.log('error fetching data',error)
+  })
+
   return (
     <>
-      <h1>Helloxs</h1>
+      <h1>`Hello my name is ${JSON.stringify(name)}`</h1>
     </>
   )
 }
